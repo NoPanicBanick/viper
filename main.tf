9ec38a364ff45e3e55b2dd5f20c10d1f121fcccc
Ryley Banick <ryley.banick@icloud.com>
2022-10-29 23:07:11 +1300

    Change the address space

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/28"]
+  address_prefixes     = ["10.0.0.0/28"]
 }
 
 resource "azurerm_public_ip" "public_ip" {
